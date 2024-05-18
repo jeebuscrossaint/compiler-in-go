f9207a5e14211b9e36de65b263e2481e0ca3e736
jeebuscrossaint <thediamond270@gmail.com>
2024-05-18 17:06:19 -0400

    file opening

--- src/main.go
+++ src/main.go
@@ -13,14 +13,22 @@ const (
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Fprintln(os.Stderr, "Fatal error encountered: no input files")
+	if len(os.Args) != 3 {
+		fmt.Fprintln(os.Stderr, "Fatal error encountered: wrong count of input files")
 		fmt.Fprintln(os.Stderr, "compilation killed.")
 		fmt.Fprintln(os.Stderr, "Usage:")
 		fmt.Fprintln(os.Stderr, "diddl <input> -o <output>")
 		os.Exit(ExitFailure)
 	} else {
+		file, err := os.Open(os.Args[1])
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Failed to open file:", err)
+			os.Exit(ExitFailure)
+		}
+		defer file.Close()
+
 		fmt.Println("Booty")
 		os.Exit(ExitSuccess)
+		// time 33:34
 	}
 }
