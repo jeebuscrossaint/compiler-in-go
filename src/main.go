b76c40a6b3b4120004f123fd4b7a7773ca966624
jeebuscrossaint <thediamond270@gmail.com>
2024-05-18 16:56:32 -0400

    cpp macro

--- src/main.go
+++ src/main.go
@@ -5,10 +5,22 @@ import (
 	"os"
 )
 
+// I NEED MY C++ MACROS BRUHHHH
+
+const (
+	ExitSuccess = 0
+	ExitFailure = 1
+)
+
 func main() {
-	if len(os.Args) > 1 {
-		fmt.Println(os.Args[1])
+	if len(os.Args) != 2 {
+		fmt.Fprintln(os.Stderr, "Fatal error encountered: no input files")
+		fmt.Fprintln(os.Stderr, "compilation killed.")
+		fmt.Fprintln(os.Stderr, "Usage:")
+		fmt.Fprintln(os.Stderr, "diddl <input> -o <output>")
+		os.Exit(ExitFailure)
 	} else {
-		fmt.Println("No file provided")
+		fmt.Println("Booty")
+		os.Exit(ExitSuccess)
 	}
 }
