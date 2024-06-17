1fc240c5b9c973fed9113fa3f5b01a0c517a33da
jeebuscrossaint <thediamond270@gmail.com>
2024-06-17 12:01:04 -0400

    implement function parsing, AND function running.

--- src/main.go
+++ src/main.go
@@ -7,6 +7,8 @@ import (
 	"os"
 	"bufio"
 	"strings"
+	"os/exec"
+	"runtime"
 )
 
 func locate_constrict(filename string) bool {
@@ -71,8 +73,60 @@ func parse_vars(filename string) (map[string]string, error) {
 } 
 
 
-func parse_functions() {
+func parse_functions(filename string) (map[string][]string, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
 
+	funcs := make(map[string][]string)
+	scanner := bufio.NewScanner(file)
+	var current_function string
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == strings.ToUpper(line) {
+			// start of a new function
+			current_function = line
+			funcs[current_function] = []string{}
+		} else if current_function != "" {
+			// part of the current function
+			funcs[current_function] = append(funcs[current_function], line)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return funcs, nil
+
+}
+
+func run_functions(funcs map[string][]string) error {
+	shell := "/bin/sh"
+	flag := "-c"
+	if runtime.GOOS == "windows" {
+		shell = "cmd"
+		flag = "/C"
+	}
+
+	for _, commands := range funcs {
+		for _, command := range commands {
+			// replace variable references with values 
+			for var_name, var_value := range vars {
+				command = strings.Replace(command, "$("+var_name+"")", var_value, -1)
+			}
+			cmd := exec.Command(shell, flag, command)
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+			err := cmd.Run()
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
 }
 
 func print_fucking_everything() {
@@ -88,16 +142,27 @@ func detect_shit() {
 }
 
 func main() {
-	file_exists := locate_constrict("confile")
-	if file_exists {
-		fmt.Println("file found")
-		vars, err := parse_vars("confile")
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(vars)
-	} else {
-		fmt.Println("file not found")
-	}
+    file_exists := locate_constrict("confile")
+    if file_exists {
+        fmt.Println("file found")
+        vars, err := parse_vars("confile")
+        if err != nil {
+            log.Fatal(err)
+        }
+        fmt.Println(vars)
+
+        funcs, err := parse_functions("confile")
+        if err != nil {
+            log.Fatal(err)
+        }
+        fmt.Println(funcs)
+
+        err = run_functions(funcs)
+        if err != nil {
+            log.Fatal(err)
+        }
+    } else {
+        fmt.Println("file not found")
+    }
 }
 
